0eb252847cd6e5ff20842fa78ae99c83d6fdf594
tgrall <tugdual@gmail.com>
2013-01-25 23:05:14 +0100

    PHP : Fix issue with counter and initial value

--- PHP/05_atomic_counters.php
+++ PHP/05_atomic_counters.php
@@ -19,9 +19,21 @@ echo "Get counter: ";
 echo $cb->get("counter");
 echo "\n";
 
-echo "Init the value to 10\n";
-// create an set an initial (increment)
-$cb->increment("counter", $initial = 10);
+
+echo "Increment by 10\n";
+$cb->increment("counter",10);
+echo "Get counter: ";
+echo $cb->get("counter");
+echo "\n";
+
+
+echo "\nDelete the counter\n";
+$cb->delete("counter");
+
+
+echo "\nInit the value to 10\n";
+// create an set an initial 
+$cb->increment("counter", 1 , true , 0 , 10);
 echo "Get counter: ";
 echo $cb->get("counter");
 echo "\n";
