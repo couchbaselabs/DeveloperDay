bd926fbb225fb13ff061abfe07c27e36caa5371b
Dustin Sallings <dustin@spy.net>
2013-01-29 19:24:14 -0800

    gofmt

--- Go/02_storage.go
+++ Go/02_storage.go
@@ -1,46 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"github.com/couchbaselabs/go-couchbase"
 	"log"
-	"fmt"
 	"reflect"
 )
 
 var db *couchbase.Bucket
 
-func outputValue(k string) (interface{}) {
-	
-	var data interface{} 
+func outputValue(k string) interface{} {
+
+	var data interface{}
 	err := db.Get(k, &data)
 
 	fmt.Printf("mykey value type = %v\n\n", reflect.TypeOf(data))
 	fmt.Printf("mykey = %v\n\n", data)
-	
-	return err	
+
+	return err
 }
 
 func main() {
-	var err interface{} 
+	var err interface{}
 	db, err = couchbase.GetBucket("http://localhost:8091/", "default", "default")
 	if err != nil {
-	    log.Fatalf("Error connecting:  %v", err)
+		log.Fatalf("Error connecting:  %v", err)
 	}
-	
+
 	err = db.Set("mykey", 0, 1)
 	if err != nil {
-	    log.Fatalf("Error setting key-value:  %v", err)
+		log.Fatalf("Error setting key-value:  %v", err)
 	}
-	
-	
-	
-	
+
 	err = outputValue("mykey")
 	if err != nil {
-	    log.Fatalf("Error setting key-value:  %v", err)
-	}	
-	
+		log.Fatalf("Error setting key-value:  %v", err)
+	}
+
 }
-
-
-
